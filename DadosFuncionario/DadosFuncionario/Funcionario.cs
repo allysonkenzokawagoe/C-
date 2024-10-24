030c6ebb3dbf773f4ab5f1aa2eadf0e348e2c03c
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2024-10-24 10:13:11 -0300

    Add files via upload
    Programa usado para ler os dados de um funcionário(nome, salário bruto e imposto). Em seguida mostrar os dados(nome, salário líquido) e aumentar o salário com base em uma porcentagem dada pelo usuário com base no salário bruto.

---
+++ DadosFuncionario/DadosFuncionario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosFuncionario {
+    internal class Funcionario {
+
+        public string nome;
+        public double salario;
+        public double imposto;
+
+        public double SalarioLiquido() {
+            return salario - imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            salario = salario + (salario * porcentagem / 100);
+        }
+
+        public override string ToString() {
+            return nome + ", $ " + SalarioLiquido();
+        }
+
+    }
+}
---
+++ DadosFuncionario/DadosFuncionario/Program.cs
@@ -0,0 +1,32 @@
+﻿using DadosFuncionario;
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionário:");
+            Console.Write("Nome: ");
+            func.nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            func.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.Write($"Funcionário: {func}");
+            Console.WriteLine(); Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+            Console.WriteLine();
+
+            Console.Write($"Dados atualizados: {func}");
+
+        }
+    }
+}
