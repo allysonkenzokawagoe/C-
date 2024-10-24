0c2e9485b634803c460c984509cdc781182db03f
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2024-10-24 10:32:55 -0300

    Add files via upload
    Programa para ler três notas de um aluno e ver se ela está aprovado ou reprovado, no caso de estar reprovado mostrar na tela quanto falta para ele passar

---
+++ AlunoNotas/AlunoNotas/Program.cs
@@ -0,0 +1,31 @@
+﻿using AlunoNotas;
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno n = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            n.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno: ");
+            n.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write($"NOTA FINAL: {n.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine();
+
+            if(n.NotaFinal() >= 60) {
+                Console.Write("APROVADO");
+            } else {
+                Console.WriteLine("REPROVADO");
+                double aprovacao = 60 - n.NotaFinal();
+                Console.WriteLine($"FALTARAM {aprovacao.ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+        }
+    }
+}
