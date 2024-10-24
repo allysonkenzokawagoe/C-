4f131cacfdc52dd6b39b136fe69d022dd80cde82
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2024-10-24 09:41:17 -0300

    Add files via upload

---
+++ Retangulo/Retangulo/Calculos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo {
+    internal class Calculos {
+
+        public double altura;
+        public double largura;
+
+        public double Area() {
+            return altura * largura;
+        }
+
+        public double Perimetro() {
+            return (largura * 2) + (altura * 2);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
+        }
+
+    }
+}
---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Retangulo;
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+ 
+            Calculos ret = new Calculos();
+
+            Console.WriteLine("Digite os valores da altura e da largura de um retângulo: ");
+            Console.Write("Altura: ");
+            ret.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Largura: ");
+            ret.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.Write($"Area = {ret.Area()}");
+            Console.WriteLine();
+            Console.Write($"Perímetro = {ret.Perimetro()}");
+            Console.WriteLine();
+            Console.Write($"Diagonal = {ret.Diagonal()}");
+
+
+        }
+    }
+}
