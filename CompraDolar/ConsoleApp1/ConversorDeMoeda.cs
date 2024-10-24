3f69dcc20978adef6562a78db92ebe40cd93cdfe
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2024-10-24 11:02:09 -0300

    Add files via upload
    Programa de comrpa de dólar segundo a cotação atual e com 6% de imposto do IOF.

---
+++ CompraDolar/ConsoleApp1/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    internal class ConversorDeMoeda {
+
+        public static double QtdReais(double cotacao, double qtd) {
+
+            return (cotacao * qtd) + (cotacao * qtd * 0.06);
+
+        }
+
+    }
+}
---
+++ CompraDolar/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write($"Valor a ser pago em reais: {ConversorDeMoeda.QtdReais(cot,qtd).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
