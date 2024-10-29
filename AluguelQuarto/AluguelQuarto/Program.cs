a3d160c0d3df00bed3850e3473191bffa34f34c2
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2024-10-29 12:12:02 -0300

    Add files via upload

---
+++ AluguelQuarto/AluguelQuarto/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace AluguelQuarto {
+    class Program {
+        static void Main(string[] args) {
+
+            Rent[] vet = new Rent[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Rent #{i}: ");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                vet[room] = new Rent(name, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy Rooms:");
+            for (int i = 0; i < 10; i++) {
+                if (vet[i] != null) {
+                    Console.WriteLine(i + ": " + vet[i]);
+                }
+            }
+        }
+    }
+}
---
+++ AluguelQuarto/AluguelQuarto/Rent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluguelQuarto {
+    internal class Rent {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Rent(string name, string email) {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Email;
+        }
+    }
+}
